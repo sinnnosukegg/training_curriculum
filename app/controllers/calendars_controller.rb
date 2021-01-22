9e8204093eea089458db263d228baa1c4e2c5e85
sinnnosukegg <shinnosukegg@icloud.com>
2021-01-22 11:07:09 +0900

    Revert "Merge pull request #8 from sinnnosukegg/second_training"
    This reverts commit 98b26354555b6c3aac77a193c546dfa2c57b719b, reversing
    changes made to b1179224019650a17b98a604952a91f64c391265.

--- app/controllers/calendars_controller.rb
+++ app/controllers/calendars_controller.rb
@@ -8,7 +8,7 @@ class CalendarsController < ApplicationController
 
   # 予定の保存
   def create
-    Plan.create(data: plan_params[:data])
+    Plan.create(plan_params)
     redirect_to action: :index
   end
 
@@ -34,16 +34,8 @@ class CalendarsController < ApplicationController
       plans.each do |plan|
         today_plans.push(plan.plan) if plan.date == @todays_date + x
       end
-
-
-      wday_num = Date.today.wday + x
-
-      if wday_num >= 7
-        wday_num = wday_num -7
-      end
-
-      days = { :month => (@todays_date + x).month, :date => (@todays_date+x).day, :plans => today_plans, :wday => wdays[wday_num]}
-
+      days = { month: (@todays_date + x).month, date: (@todays_date+x).day, plans: today_plans}
+      @week_days.push(days)
     end
 
   end
